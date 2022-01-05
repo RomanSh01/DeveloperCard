600a3b2f61c3aabb5f5306b68262cb678f822336
Roman <r-shabardin@yandex.ua>
2022-01-05 19:47:05 +0100

    Initial Commit

---
+++ DeveloperCard/DeveloperCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperCardApp.swift
+//  DeveloperCard
+//
+//  Created by Роман Шабардин on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
